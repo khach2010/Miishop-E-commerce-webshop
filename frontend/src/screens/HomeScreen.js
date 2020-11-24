fe254e9d60fd2c43dfc405a98313c44ed2a6c161
tu le <tule@tus-MacBook-Pro.local>
2020-11-24 18:48:52 +0100

    install and use axios to fetch products from the backend

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Row, Col } from 'react-bootstrap'
-import products from '../products'
+import axios from 'axios'
 import Product from '../components/Product'
 
 const HomeScreen = () => {
+  const [products, setProducts] = useState([])
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const res = await axios.get('/api/products')
+      setProducts(res.data)
+    }
+    fetchProducts()
+  }, [])
+
   return (
     <>
       <h1>Latest product</h1>
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import { Link } from 'react-router-dom'
 import {
   Row,
@@ -10,10 +11,17 @@ import {
   ListGroupItem,
 } from 'react-bootstrap'
 import Rating from '../components/Rating'
-import products from '../products'
 
 const ProductScreen = ({ match }) => {
-  const product = products.find((p) => p._id === match.params.id)
+  const [product, setProduct] = useState({})
+
+  useEffect(() => {
+    const fetchProduct = async () => {
+      const res = await axios.get(`/api/products/${match.params.id}`)
+      setProduct(res.data)
+    }
+    fetchProduct()
+  }, [])
 
   return (
     <>
