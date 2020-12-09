1032eb2b8a4477bb9c12f917fcfd5a071d7492d0
tu le <tule@tus-MacBook-Pro.local>
2020-12-09 11:47:27 +0100

    user register reducer action & screen

--- frontend/src/screens/LoginScreen.js
+++ frontend/src/screens/LoginScreen.js
@@ -61,14 +61,12 @@ const LoginScreen = ({ location, history }) => {
 
         <Row className="py-3">
           <Col>
-            New Customer ?{' '}
-            {
-              <Link
-                to={redirect ? `/register?redirect=${redirect}` : '/register'}
-              >
-                Register
-              </Link>
-            }
+            New Customer ?
+            <Link
+              to={redirect ? `/register?redirect=${redirect}` : '/register'}
+            >
+              Register
+            </Link>
           </Col>
         </Row>
       </Form>
