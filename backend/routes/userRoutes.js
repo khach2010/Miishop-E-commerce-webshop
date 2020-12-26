fecab826aaf595137f4d29f9315c74665d808cb5
tu le <tule@tus-MacBook-Pro.local>
2020-12-26 12:02:04 +0100

    get user by ID & update user endpoints

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -8,16 +8,21 @@ import {
   updateUserProfile,
   getUsers,
   deleteUser,
+  getUserById,
+  updateUser,
 } from '../controllers/userController.js'
 import { protect, admin } from '../middleware/authMiddleware.js'
 
 router.route('/').post(registerUser)
 router.route('/').get(protect, admin, getUsers)
+
 router.post('/login', authUser)
-router
-  .route('/profile')
-  .get(protect, getUserProfile)
-  .put(protect, updateUserProfile)
+
+router.route('/profile').get(protect, getUserProfile)
+router.route('/profile').put(protect, updateUserProfile)
 
 router.route('/:id').delete(protect, admin, deleteUser)
+router.route('/:id').get(protect, admin, getUserById)
+router.route('/:id').put(protect, admin, updateUser)
+
 export default router
