0a010e5447becc2dc95e26e1fd1619b82ab8dace
tu le <tule@tus-MacBook-Pro.local>
2020-12-30 17:28:31 +0100

    Admin edit product screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -16,6 +16,7 @@ import OrderScreen from './screens/OrderScreen'
 import UserListScreen from './screens/UserListScreen'
 import UserEditScreen from './screens/UserEditScreen'
 import ProductListScreen from './screens/ProductListScreen'
+import ProductEditScreen from './screens/PorductEditScreen'
 
 function App() {
   return (
@@ -35,6 +36,7 @@ function App() {
           <Route path="/admin/userlist" component={UserListScreen} />
           <Route path="/admin/user/:id/edit" component={UserEditScreen} />
           <Route path="/admin/productlist" component={ProductListScreen} />
+          <Route path="/admin/product/:id/edit" component={ProductEditScreen} />
           <Route exact path="/" component={HomeScreen} />
         </main>
       </Container>
