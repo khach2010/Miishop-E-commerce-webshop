d4d730784d5cf6a42672a29130fe264486f32573
tu le <tule@tus-MacBook-Pro.local>
2021-01-03 06:54:09 +0100

    Image Upload

--- backend/routes/uploadRoutes.js
+++ backend/routes/uploadRoutes.js
@@ -17,15 +17,13 @@ const storage = multer.diskStorage({
 
 function checkFileType(file, cb) {
   const filetypes = /jpg|jpeg|png/
-  const extname = filetypes.test(
-    path.extname(file.originalname).toLocaleLowerCase()
-  )
+  const extname = filetypes.test(path.extname(file.originalname).toLowerCase())
   const mimetype = filetypes.test(file.mimetype)
 
   if (extname && mimetype) {
-    return cd(null, true)
+    return cb(null, true)
   } else {
-    cb('Image only')
+    cb('Images only!')
   }
 }
 
@@ -37,7 +35,7 @@ const upload = multer({
 })
 
 router.post('/', upload.single('image'), (req, res) => {
-  res.send(`${req.file.path}`)
+  res.send(`/${req.file.path}`)
 })
 
 export default router
