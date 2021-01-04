67f610686f22300e973487dc4fcd638323b96372
tu le <tule@tus-MacBook-Pro.local>
2021-01-04 09:49:46 +0100

    mark order as delivered

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -7,6 +7,7 @@ import {
   updateOrderToPaid,
   getMyOrders,
   getOrders,
+  updateOrderToDelivered,
 } from '../controllers/orderController.js'
 import { protect, admin } from '../middleware/authMiddleware.js'
 
@@ -15,5 +16,6 @@ router.route('/').get(protect, admin, getOrders)
 router.route('/myorders').get(protect, getMyOrders)
 router.route('/:id').get(protect, getOrderById)
 router.route('/:id/pay').put(protect, updateOrderToPaid)
+router.route('/:id/deliver').put(protect, admin, updateOrderToDelivered)
 
 export default router
